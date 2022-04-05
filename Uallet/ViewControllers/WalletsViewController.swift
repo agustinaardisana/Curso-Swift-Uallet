2fb5f962fae64ab33de5e2c6f0c37100e5d1f08c
Agustina Ardisana <agustina.ardisana@uala.com.ar>
2022-04-05 17:27:45 -0300

    mostrar tableView con celdas dummy

--- Uallet/ViewControllers/WalletsViewController.swift
+++ Uallet/ViewControllers/WalletsViewController.swift
@@ -2,23 +2,102 @@
 //  WalletsViewController.swift
 //  Uallet
 //
-//  Created by Agustina Ardisana on 29/03/2022.
+//  Created by Agustina Ardisana on 05/04/2022.
 //
 
 import UIKit
 
-class WalletsViewController: UIViewController {
-
+class WalletsViewController: UITableViewController {
+    
+    let cellIdentifier = "celdaWallets"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(agregarWallet))
+        
+        // Uncomment the following line to preserve selection between presentations
+         self.clearsSelectionOnViewWillAppear = false
+        
+        // Registrar una celda custom
+        let nib = UINib(nibName: "WalletViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: cellIdentifier)
     }
     
+    //MARK: Eventos de UI
+    
     @objc func agregarWallet() {
-        let nuevaWalletVC = NuevaWalletViewController()
-        let nuevaWalletNavigationVC = UINavigationController(rootViewController: nuevaWalletVC)
-        present(nuevaWalletNavigationVC, animated: true)
+           let nuevaWalletVC = NuevaWalletViewController()
+           let nuevaWalletNavigationVC = UINavigationController(rootViewController: nuevaWalletVC)
+           present(nuevaWalletNavigationVC, animated: true)
+       }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return WalletsStorage.shared.wallets.count
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier,
+                                                 for: indexPath) as! WalletViewCell
+        
+        let wallet = WalletsStorage.shared.wallets[indexPath.row]
+        
+        cell.textLabel!.text = wallet.nombre
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
 }
