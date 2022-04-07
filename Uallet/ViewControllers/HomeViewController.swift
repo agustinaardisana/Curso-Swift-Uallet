5650dc05db667f476ce670002a7b1d301b8bced5
Agustina Ardisana <agustina.ardisana@uala.com.ar>
2022-04-07 17:42:05 -0300

    mostrar ballets totales en home

--- Uallet/ViewControllers/HomeViewController.swift
+++ Uallet/ViewControllers/HomeViewController.swift
@@ -9,12 +9,23 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var lblTotalWallets: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Home"
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Log out", style: .plain, target: self, action: #selector(logout))
+        
+        mostrarCantidadWallets()
+        
+        WalletsStorage.shared.actualizarInfo {
+            self.mostrarCantidadWallets()
+        }
+    }
     
+    func mostrarCantidadWallets() {
+        lblTotalWallets.text = "Tienes \(WalletsStorage.shared.wallets.count) wallets"
     }
     @objc func logout() {
         UserDefaults.standard.set(false, forKey: "logueado")
